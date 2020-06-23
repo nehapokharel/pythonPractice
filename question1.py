2526d7966b309541063b5a53af751ccf72e21e8b
nehapokharel <nehapokharel008@gmail.com>
2020-06-23 21:10:47 +0545

    solution of question 1

---
+++ question1.py
@@ -0,0 +1,11 @@
+word = input('enter string: ')
+result={}
+
+for i in word:
+    if i not in result.keys():
+        result[i]=0
+    result[i] += 1
+
+sorted_keys = sorted(result, key=result.get, reverse=True)
+print({values:result[values] for values in sorted_keys})
+   
