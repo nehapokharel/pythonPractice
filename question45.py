6d2563e84abced99e18ef886044290fbd9d3168a
nehapokharel <nehapokharel008@gmail.com>
2020-06-27 21:23:50 +0545

    solution of question 45

---
+++ question45.py
@@ -0,0 +1,9 @@
+tuple_values = ('p', 'y', 't', 'h', 'o', 'n') 
+
+print("Some examples showing index of item in tuple: ")
+
+print(tuple_values.index('y'))
+
+print(tuple_values.index('n'))
+
+print(tuple_values.index('h'))
